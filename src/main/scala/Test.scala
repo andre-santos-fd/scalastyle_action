6627074b3688089e84d9307f2ce752d6e2b7cede
André Santos <103038983+andre-santos-fd@users.noreply.github.com>
2022-05-05 12:27:59 +0100

    Scala code (#3)
    * adding gitignore
    
    * Adding scala sample code
    
    * Adding scala sample code

---
+++ src/main/scala/Test.scala
@@ -0,0 +1,23 @@
+class Test {
+
+  class Point {
+    private var _x = 0
+    private var _y = 0
+    private val bound = 100
+
+    def x: Int = _x
+
+    def x_=(newValue: Int): Unit = {
+      if (newValue < bound) _x = newValue else printWarning()
+    }
+
+    def y: Int = _y
+
+    def y_=(newValue: Int): Unit = {
+      if (newValue < bound) _y = newValue else printWarning()
+    }
+
+    private def printWarning() = println("WARNING: Out of bounds")
+  }
+
+}
