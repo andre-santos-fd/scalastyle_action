f9c72bfaf0becf9bd8abbd1d149eceeb45b7b41c
André Santos <andre.santos@fanduel.com>
2022-05-05 12:24:42 +0100

    Adding scala sample code

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -6,15 +6,18 @@ class Test {
     private val bound = 100
 
     def x: Int = _x
-    def x_= (newValue: Int): Unit = {
+
+    def x_=(newValue: Int): Unit = {
       if (newValue < bound) _x = newValue else printWarning()
     }
 
     def y: Int = _y
-    def y_= (newValue: Int): Unit = {
+
+    def y_=(newValue: Int): Unit = {
       if (newValue < bound) _y = newValue else printWarning()
     }
 
     private def printWarning() = println("WARNING: Out of bounds")
+  }
 
 }
