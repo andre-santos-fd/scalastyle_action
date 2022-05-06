22646f9ca4f409690fd5aec15e932ac125205bcc
André Santos <andre.santos@fanduel.com>
2022-05-06 11:42:49 +0100

    Adding sbt scalafix

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,8 @@ if [ ${INPUT_DEBUG} == "true" ]; then
   exec java -jar /scalastyle.jar --config /scalastyle_config.xml "${INPUT_WORKDIR}"
 fi
 
-exec java -jar /scalastyle.jar --config /scalastyle_config.xml "${INPUT_WORKDIR}" \
- | reviewdog -f=scalastyle -diff="git diff FETCH_HEAD" \
+exec  sbt clean compile scalafix \
+ | reviewdog -f=scalastyle \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-check}" \
       -filter-mode="${INPUT_FILTER_MODE:-added}" \
